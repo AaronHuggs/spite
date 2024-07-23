accace84904e57dd29e2e0fe85385800f8c24ac7
AaronHuggs <huggins.aaron@outlook.com>
2024-07-23 11:32:18 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const drawButton = document.getElementById('draw-button');
+
+    drawButton.addEventListener('click', () => {
+        // Implement draw card functionality
+        alert('Draw card functionality to be implemented.');
+    });
+});
